8700673e474bc657f11d527cdd41859af9d214e8
ceydaoymak <22070006012@stu.yasar.edu.tr>
2025-02-05 13:57:15 +0300

    Initial Commit

---
+++ Kelimeyi Bul/Kelimeyi_BulApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kelimeyi_BulApp.swift
+//  Kelimeyi Bul
+//
+//  Created by ceyda oymak on 5.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Kelimeyi_BulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kelimeyi BulUITests/Kelimeyi_BulUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Kelimeyi_BulUITestsLaunchTests.swift
+//  Kelimeyi BulUITests
+//
+//  Created by ceyda oymak on 5.02.2025.
+//
+
+import XCTest
+
+final class Kelimeyi_BulUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
